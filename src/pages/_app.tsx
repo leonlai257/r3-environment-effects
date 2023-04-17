82a375fefe7cf003bd7717864d3d0e78944cf76e
Leon LAI <leonlai257@gmail.com>
2023-04-17 17:44:49 +0800

    feat(init): init project with prebuilt world

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,44 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Canvas } from '@react-three/fiber';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Route, Switch } from 'wouter';
+import React from 'react';
+import Main from '.';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ pageProps }: AppProps) {
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                overflow: 'hidden',
+            }}>
+            <Head>
+                <title>React three Environment</title>
+                <meta
+                    name="React three Environment"
+                    content="React three Environment by Leon"
+                />
+            </Head>
+            <Canvas shadows={'soft'}>
+                <ambientLight castShadow intensity={0.1} />
+
+                <directionalLight
+                    intensity={0.5}
+                    castShadow
+                    shadow-mapSize-height={128}
+                    shadow-mapSize-width={128}
+                />
+                <Switch>
+                    <Route path="/">
+                        <Main {...pageProps} />
+                    </Route>
+                </Switch>
+            </Canvas>
+        </div>
+    );
 }
